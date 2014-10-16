fc2cd6972571d2eca4dd43279b67f6013cd4fb69
David Wilson <daw4475@g.rit.edu>
2014-10-16 13:50:51 -0400

    raw camera control and character movement. hopefully the gitignore works now....

--- Topaz/Assets/Scripts/Player/CharacterController.cs
+++ Topaz/Assets/Scripts/Player/CharacterController.cs
@@ -12,16 +12,30 @@ namespace Character
         public float translationSpeed = 10.0f;
         public float rotationSpeed = 50.0f;
 
+        float minRotation = -25f;
+        float maxRotation = 25f;
+
+        Camera characterCam;
+        float rotationY = 0f;
+        float rotationX = 0f;
+
         void Start()
         {
-            
+            characterCam = GetComponentInChildren<Camera>();
         }
 
         void Update()
         {
             transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * translationSpeed * Time.deltaTime);
-            transform.Rotate(Vector3.up, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime);
-            //transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime);
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * translationSpeed * Time.deltaTime);
+
+            rotationX += Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
+            rotationY += Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+
+            rotationX = Mathf.Clamp(rotationX, minRotation, maxRotation);
+            rotationY = Mathf.Clamp(rotationY, minRotation, maxRotation);
+
+            characterCam.transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
         }
     }
 }
